9e0236219f90b93ae6116d64aa2f73c7fc1b83ce
Naufal Ardra Anabil <118241118+naufalanabil@users.noreply.github.com>
2022-12-28 19:08:09 +0700

    Variable.py

---
+++ Variable.py
@@ -0,0 +1,12 @@
+# Menginput Nilai Variabel
+x = input('Tuliskan nilai x: ')
+y = input('Tuliskan nilai y: ')
+ 
+# Membuat Variabel tukar dan Menukar nilai Variabel lain
+tukar = x
+x = y
+y = tukar
+ 
+#Menampilkan Nilai Variabel Setelah Ditukar
+print('Nilai x Setelah Ditukar adalah: {}'.format(x))
+print('Nilai y Setelah Ditukar adalah: {}'.format(y))
