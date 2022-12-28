66cf061f2560c074b88484f9f055fbe4ad6a575e
Naufal Ardra Anabil <118241118+naufalanabil@users.noreply.github.com>
2022-12-28 19:04:37 +0700

    Positive, Negative, OR Zero.py

---
+++ Positive, Negative, OR Zero.py
@@ -0,0 +1,14 @@
+#Menginput Angka
+angka = float(input("Tulis Sebuah Angka: "))
+ 
+#Menampilkan Kondisi Angka Positif
+if angka > 0:
+   print("Angka Positif")
+ 
+#Menampilkan Kondisi Angka Nol   
+elif angka == 0:
+   print("Angka Nol")
+ 
+#Menampilkan Kondisi Angka Negatif
+else:
+   print("Angka Negatif")
